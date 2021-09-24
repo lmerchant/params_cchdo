1f177d04099ca300440267ca10290e141371e237
DocOtak <DocOtak@users.noreply.github.com>
2021-09-24 22:00:38 +0000

    deploy: bad23fc11bd3810b0fc8085ef4b45fe0005681d8

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.15.dev2+ge9d613b',
+    VERSION: '0.1.15.dev3+gbad23fc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
