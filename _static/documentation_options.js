d8cc244f7b39fa7adde3e81fe89d9d599cb00f28
DocOtak <DocOtak@users.noreply.github.com>
2022-06-11 22:32:30 +0000

    deploy: 4501addb3a76b01a12133c23d6dd4f05b955e8aa

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.18.dev7+g9d399f2',
-    LANGUAGE: 'None',
+    VERSION: '0.1.18.dev8+g4501add',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
@@ -10,5 +10,5 @@ var DOCUMENTATION_OPTIONS = {
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: true,
+    ENABLE_SEARCH_SHORTCUTS: false,
 };
