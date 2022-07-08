9ac6cb5c779a48fbc005476247b20fc8f343b2a1
DocOtak <DocOtak@users.noreply.github.com>
2022-07-08 23:25:44 +0000

    deploy: a08d219437034fc47f28056357004f0cf4de4314

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.18.dev27+g70a6ff9',
+    VERSION: '0.1.18.dev28+ga08d219',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
